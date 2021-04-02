25c7aefa05c41d14776f633e12c8a830fcb000a8
Ashwin Balaje <ashwin.balaje@dcmail.ca>
2021-04-01 22:53:34 -0400

    Modifed styling

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+
 import ScratchCard from 'react-scratchcard';
-import image from './tokyoghoul.jpg';
+import image from './assets/images/scratch.png';
 // import ScratchOff from './ScratchOff';
 // import image from './tokyoghoul.jpg'
 
@@ -37,7 +39,7 @@ function Game() {
 
     const settings = {
         width: 500,
-        height: 500,
+        height: 483,
         image: image,
         finishPercent: 100,
     };
@@ -49,9 +51,24 @@ function Game() {
                 <div className="large-4 small-10 small-offset-1 cell titleText gameTitle">
                     <h1 id="">Let's Win Some Prizes!</h1>
                     <p>Rules</p>
+                    <ul>
+                        <li>Scratch the card to win amazing prizes.</li>
+                        <li>If you have won a prize, you will be redirected to a skill testing page. If answered correctly, your prize will be added to your account.</li>
+                        <li>You will be awarded the prize you won in 6 to 8 weeks.</li>
+                        <li>You can only play the game again once every 72 hours.</li>
+                        <li>For further details, please refer to our <Link to="/legal">Terms and Conditions & Privacy Policy</Link></li>
+                    </ul>
+
+                    <p>Prizes</p>
+                    <ul>
+                        <li>1 Prize of 10 000 BuyMore Dollars</li>
+                        <li>5 Prizes of 750 BuyMore Dollars</li>
+                        <li>10 Prizes of 100 BuyMore Dollars</li>
+                        <li>100 Prizes of 20 BuyMore Dollars</li>
+                    </ul>
                 </div>
 
-                <div className="large-6 large-offset-1 small-10 small-offset-1 cell formBox">
+                <div className="large-6 large-offset-1 small-10 small-offset-1 cell gameBox">
                     <h2>Lucky Scratch</h2>
                     <ScratchCard {...settings} onLoad={check()}>
                         {msg}
--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,14 @@ function Home(props){
             <div className="grid-x grid-padding-x">
 
                 
-                <div className="large-10 large-offset-1 small-10 small-offset-1 cell confirmBox">
-                    <h2> </h2>
+                <div className="large-10 large-offset-1 small-10 small-offset-1 cell homeBox">
+                    <h2>Home</h2>
+                    <p>Let's play and win some prizes.</p>
+                    <button id="confirmationBtn" className="submitBtn" ><p>Continue<span>&#62;</span></p></button>
+                </div>
+
+                <div className="large-10 large-offset-1 small-10 small-offset-1 cell homeBox">
+                    <h2>News</h2>
                     <p>Thanks for registration. Let's play and win some prizes.</p>
                     <button id="confirmationBtn" className="submitBtn" ><p>Continue<span>&#62;</span></p></button>
                 </div>
